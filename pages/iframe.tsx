9b4338d6119b61a9b624943b16a960cacba49dd0
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2023-09-21 17:00:40 -0300

    adding site

---
+++ pages/iframe.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const IframePage = () => {
+	return (
+    <main>
+        <div className='iframe'>
+            <h2>Hello from the Iframe Page</h2>
+            <p>This content can be embedded in an iframe.</p>
+        </div>
+    </main>
+	);
+};
+
+export default IframePage;
