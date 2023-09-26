7a5d40b5198176c22c60134f10d50db575e529b2
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2023-09-26 10:54:10 -0300

    ancestor

--- next.config.js
+++ next.config.js
@@ -2,19 +2,19 @@
 /** @type {import('helmet').HelmetOptions} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers(){
+  async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'http://localhost:3000/'"
-          }
-        ]
-      }
-    ]
-  }
+            value: "frame-ancestors 'http://localhost:3000/'",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 
