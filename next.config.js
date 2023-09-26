9188fcb552128d43ec0658c383db587ac9ab103a
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2023-09-26 11:17:28 -0300

    ancestor

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' 'https://iframetobeimported.netlify.app/iframe'",
+            value: "frame-ancestors 'self' http://localhost:3000/",
           },
         ],
       },
