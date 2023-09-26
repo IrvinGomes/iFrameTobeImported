4f8d98143664f4f9d3d0e70252d7e84865a2cc22
irvin.r.gomes@outlook.com <irvin.gomes@points.com>
2023-09-26 10:51:10 -0300

    ancestor

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('helmet').HelmetOptions} */
 const nextConfig = {
   reactStrictMode: true,
-   async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000/" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
+  async headers(){
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'http://localhost:3000/'"
+          }
         ]
-    }
+      }
+    ]
+  }
 }
+
 
 module.exports = nextConfig
